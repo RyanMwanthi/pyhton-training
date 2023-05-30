28aa1e565917a90147b546eb58ed58fc269d9c78
RyanMwanthi <ryamwanthi@gmail.com>
2023-05-30 10:22:45 +0300

    git commit m "changes"

--- myquestion.py
+++ myquestion.py
@@ -4,13 +4,33 @@ h=(int(input("enterh:")))
 area=b*h/2
 print(area)
 
+#3 phone number
+phone_number = input("Enter number: ")
 
+if phone_number.startswith("+254"):
+    print("Valid format.")
+else:
+    if phone_number.startswith("07"):
+        phone_number = "+254" + phone_number[1:]
+    elif phone_number.startswith("71"):
+        phone_number = "+254" + phone_number[1:]
+    elif phone_number.startswith("254"):
+        phone_number = "+254" + phone_number[3:]
+    elif phone_number.startswith("01"):
+        phone_number = "+254" + phone_number[1:]
+    else:
+        print("Invalid phone number.")
+        exit()
 
+    print(phone_number)
 
-
-
-
-
+#4 Email
+email = str(input("enter the email: "))
+email.strip().lower()
+if "@" and "." in  email[1:]:
+    print("Correct format", email)
+else:
+    print("invalid email")
 
 
 #5
